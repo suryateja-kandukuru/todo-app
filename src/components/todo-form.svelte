8d5699bedd297377127f94f95cda1b3c091b8c4c
suryateja-kandukuru <70886018+suryateja-kandukuru@users.noreply.github.com>
2023-01-22 01:00:47 +0530

    toast added!

--- src/components/todo-form.svelte
+++ src/components/todo-form.svelte
@@ -9,13 +9,23 @@
   {#if $todoForm.hasError('todo.required')}
     <Helper color="red" helperClass='mb-4'>*Todo is required.</Helper>
   {/if}
-  
+
   <Dropdown dropdownOptions="{ todoStatus }" dropdownName="{ dropdownName }" on:dropdownChange={ handleDropdownChange } />
   <Button class="m-4" type="submit" disabled={!$todoForm.valid} on:click={ handleTodoSave }>Save</Button>
 </form>
 
+{#if showToast}
+<Toast simple={true} position="top-right">
+  <svelte:fragment slot="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" /></svg>
+  </svelte:fragment>
+  Todo saved to store.
+</Toast>
+{/if}
+
+
 <script lang="ts">
-  import { Input, Label, Helper, Button } from 'flowbite-svelte';
+  import { Input, Helper, Button, Toast } from 'flowbite-svelte';
 
   import { form, field } from 'svelte-forms';
   import { required } from 'svelte-forms/validators';
@@ -24,6 +34,8 @@
 
   // variables
   let dropdownName = 'high'
+  let showToast = false
+
   const todoStatus: string[] = ['high', 'medium', 'low']
   const todo = field('todo', '', [required()]);
   const todoForm = form(todo);
@@ -39,6 +51,14 @@
     })
     setRandomPriority()
     todoForm.reset()
+    toggleToast()
+  }
+
+  const toggleToast = () => {
+    showToast = true
+    setTimeout(() => {
+      showToast = false
+    }, 2000)
   }
 
   const setRandomPriority = () => {
@@ -48,6 +68,5 @@
     } else {      
       dropdownName = todoStatus[random]
     }
-
   }
 </script>
